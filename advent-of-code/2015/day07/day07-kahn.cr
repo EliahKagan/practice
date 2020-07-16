5f98475ac4602cbf831a81681fadcc5f939cb183
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-16 14:33:41 -0400

    draft Graph + HashGraph implementations

--- advent-of-code/2015/day07/day07-kahn.cr
+++ advent-of-code/2015/day07/day07-kahn.cr
@@ -1,19 +1,19 @@
 # Advent of code 2015, part A
 # Via bottom-up toposort via Kahn's algorithm. (Cycle checking?)
 
-# Exception thrown when attempting a reentrant or otherwise repeated
-# topological sort on a Graph (or a HashGraph, which uses Graph).
-class AlreadyToposortingError
-  def initialize
-    super("Topological sort already started")
-  end
+require "deque"
+
+# Exception thrown when a Graph is in a state that doesn't support a requested
+# operation.
+class GraphError
 end
 
 # Integer-vertex adjacency-list-based digraph, for topological sort.
 class Graph(T)
   @adj = Array(Array(Int32)).new
   @indegrees = Array(Int32).new
-  @started_toposort = false
+  @toposort_started = false
+  @toposort_finished = false
 
   # The number of vertices in the graph.
   def order
@@ -44,10 +44,41 @@ class Graph(T)
   end
 
   def toposort
-    raise AlreadyToposortingError if @started_toposort
-    @started_toposort = true
+    raise GraphError("Topological sort already started") if @toposort_started
+    @toposort_started = true
 
-    roots =
+    queue = roots
+
+    until queue.empty?
+      src = queue.shift
+
+      @adj[src].each do |dest|
+        @indegrees[dest] -= 1
+        queue.push(dest) if @indegrees[dest].zero?
+      end
+
+      yield src
+    end
+
+    @toposort_finished = true
+  end
+
+  def cycle_vertices
+    raise GraphError("topological sort not finished") unless @toposort_finished
+
+    @indegrees.each_with_index do |indegree, vertex|
+      yield vertex unless indegree.zero?
+    end
+  end
+
+  private def roots
+    queue = Deque(Int32).new
+
+    @indegrees.each_with_index do |indegree, vertex|
+      queue.push(vertex) if indegree.zero?
+    end
+
+    queue
   end
 end
 
@@ -57,7 +88,42 @@ class HashGraph(T)
   @keys_by_index = Array(Int32).new # maps indices to keys
   @graph = Graph(T).new # underlying graph whose vertices are indices
 
-  # FIXME: implement the rest
+  def order
+    @graph.order
+  end
+
+  def has_vertex?(key)
+    @indices_by_key.has_key?(key)
+  end
+
+  def add_edge(src, dest)
+    @graph.add_edge(index(src), index(dest))
+  end
+
+  def toposort
+    @graph.toposort { |index| yield @keys_by_index[index] }
+  end
+
+  def cycle_vertices
+    @graph.cycle_vertices { |index| yield @keys_by_index[index] }
+  end
+
+  # Looks up the index for a key, adding it with the next available index if
+  # the key does not already identify a vertex in the graph.
+  private def index(key)
+    index = @indices_by_key[key]?
+    return index if index
+
+    index = @graph.make_vertex
+    @keys_by_index << key
+    @indices_by_key[key] = index
+
+    unless @graph.order == @keys_by_index.size == @indices_by_key.size
+      raise "Internal error: #{self.class} seems to have lost a vertex"
+    end
+
+    index
+  end
 end
 
 
