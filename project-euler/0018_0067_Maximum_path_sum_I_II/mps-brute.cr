d7a7c3d4d60da00310c583b70b7d618252ffcef3
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-30 11:44:53 -0400

    extracted grid shape validation to its own method

--- project-euler/0018_0067_Maximum_path_sum_I_II/mps-brute.cr
+++ project-euler/0018_0067_Maximum_path_sum_I_II/mps-brute.cr
@@ -1,10 +1,10 @@
+def correct_shape?(grid)
+  grid.each_with_index(1).all? { |row, count| row.size == count }
+end
+
 def read_grid(io = ARGF)
   grid = io.each_line.map(&.split.map(&.to_i)).to_a
-  
-  unless grid.each_with_index(1).all? { |row, count| row.size == count }
-    raise "wrong grid shape"
-  end
-  
+  raise "wrong grid shape" unless correct_shape?(grid)
   grid
 end
 
