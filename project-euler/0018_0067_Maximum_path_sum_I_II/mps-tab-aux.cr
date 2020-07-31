50b3c63a01ab49d520631a58745329083c5bc547
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-30 23:32:05 -0400

    version that tabulates in auxiliary storage - crash bug

---
+++ project-euler/0018_0067_Maximum_path_sum_I_II/mps-tab-aux.cr
@@ -0,0 +1,23 @@
+def correct_shape?(grid)
+  grid.each_with_index(1).all? { |row, count| row.size == count }
+end
+
+def read_grid(io = ARGF)
+  grid = io.each_line.map(&.split.map(&.to_i)).to_a
+  raise "wrong grid shape" unless correct_shape?(grid)
+  grid
+end
+
+def max_path_sum(grid)
+  cur = grid.last.clone
+  nxt = Array(Int32).new(grid.size)
+
+  (grid.size - 2).downto(0) do |i|
+    0.upto(i) { |j| nxt[j] = grid[i][j] + Math.max(cur[j], cur[j + 1]) }
+    cur, nxt = nxt, cur
+  end
+
+  cur[0]
+end
+
+puts max_path_sum(read_grid)
