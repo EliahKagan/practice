f6996c7a3ca147d094934fdc30a173333f6c0c85
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-13 04:50:12 -0400

    Running Median, translated to Ruby, passed

--- hackerrank/data-structures/heap/find-the-running-median.rb
+++ hackerrank/data-structures/heap/find-the-running-median.rb
@@ -1,23 +1,35 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 # https://www.hackerrank.com/challenges/find-the-running-median
 # With a maxheap of lower values and a minheap of higher values.
+
+$VERBOSE = 1
+
+# Array extension for swapping elements.
+class Array
+  def swap(index1, index2)
+    self[index1], self[index2] = self[index2], self[index1]
+  end
+end
 
 # Signals an invalid operation on a PriorityQueue.
 class PriorityQueueError < IndexError
 end
 
 # A priority queue implemented as a binary heap. With <=>, this is a minheap.
-class PriorityQueue(T)
+class PriorityQueue
   def self.make_minheap
-    PriorityQueue(T).new { |ls, rs| ls <=> rs }
+    PriorityQueue.new { |ls, rs| ls <=> rs }
   end
 
   def self.make_maxheap
-    PriorityQueue(T).new { |ls, rs| rs <=> ls }
+    PriorityQueue.new { |ls, rs| rs <=> ls }
   end
 
-  @elems = [] of T
-
-  def initialize(&@comparer : T, T -> Int32)
+  def initialize(&block)
+    @comparer = block
+    @elems = []
   end
 
   def empty?
@@ -28,7 +40,7 @@ class PriorityQueue(T)
     @elems.size
   end
 
-  def push(key : T)
+  def push(key)
     @elems << key
     sift_up(size - 1)
   end
@@ -36,7 +48,7 @@ class PriorityQueue(T)
   def top
     @elems[0]
   rescue IndexError
-    raise PriorityQueueError.new("can't read top of empty priority queue")
+    raise PriorityQueueError, "can't read top of empty priority queue"
   end
 
   def pop
@@ -50,42 +62,49 @@ class PriorityQueue(T)
     ret
   end
 
-  private def sift_up(child)
+  private
+
+  def sift_up(child)
     until child.zero?
-      parent = (child - 1) // 2
+      parent = (child - 1) / 2
       break if order_ok?(parent, child)
+
       @elems.swap(parent, child)
       child = parent
     end
   end
 
-  private def sift_down(parent)
+  def sift_down(parent)
     loop do
       child = pick_child(parent)
       break if child.nil? || order_ok?(parent, child)
+
       @elems.swap(parent, child)
       parent = child
     end
   end
 
-  private def pick_child(parent)
+  def pick_child(parent)
     left = parent * 2 + 1
     return nil if left >= size
+
     right = left + 1
     right == size || order_ok?(left, right) ? left : right
   end
 
-  private def order_ok?(parent, child)
+  def order_ok?(parent, child)
     @comparer.call(@elems[parent], @elems[child]) <= 0
   end
 end
 
 # A growing bag of numbers that efficiently computes medians.
 class MedianBag
-  @lows = PriorityQueue(Int32).make_maxheap
-  @highs = PriorityQueue(Int32).make_minheap
+  def initialize
+    @lows = PriorityQueue.make_maxheap
+    @highs = PriorityQueue.make_minheap
+  end
 
-  def push(value : Int32)
+  def push(value)
     if @highs.empty? || value < @highs.top
       @lows.push(value)
     else
@@ -99,15 +118,17 @@ class MedianBag
     when -1
       @lows.top.to_f
     when 0
-      (@lows.top + @highs.top) / 2
+      (@lows.top + @highs.top) / 2.0
     when +1
       @highs.top.to_f
     else
-      raise "Bug: balancing invariant violated"
+      raise 'Bug: balancing invariant violated'
     end
   end
 
-  private def rebalance
+  private
+
+  def rebalance
     case balance_factor
     when -2
       @highs.push(@lows.pop)
@@ -116,18 +137,16 @@ class MedianBag
     end
   end
 
-  private def balance_factor
+  def balance_factor
     @highs.size - @lows.size
   end
 end
 
-def read_value
-  gets.as(String).to_i
-end
+if __FILE__ == $PROGRAM_NAME
+  bag = MedianBag.new
 
-bag = MedianBag.new
-
-read_value.times do
-  bag.push(read_value)
-  printf "%.1f\n", bag.median
+  gets.to_i.times do
+    bag.push(gets.to_i)
+    printf "%.1f\n", bag.median # rubocop:disable Style/FormatStringToken
+  end
 end
