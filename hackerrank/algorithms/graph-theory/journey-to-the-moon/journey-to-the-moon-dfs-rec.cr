2f72906b42d496ee1a260c7e17019d0b619b9e5c
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-17 02:34:23 -0400

    Journey, recursive DFS, in Crystal

---
+++ hackerrank/algorithms/graph-theory/journey-to-the-moon/journey-to-the-moon-dfs-rec.cr
@@ -0,0 +1,65 @@
+# https://www.hackerrank.com/challenges/journey-to-the-moon
+# In Crystal. Using recursive depth-first search.
+
+require "bit_array"
+
+# Adjacency-list representation of an unweighted undirected graph.
+class Graph
+  @adj : Array(Array(Int32))
+
+  def initialize(order : Int32)
+    @adj = (0...order).map { [] of Int32 }
+  end
+
+  def add_edge(u, v)
+    raise IndexError.new("edge u is out of range") unless exists?(u)
+    raise IndexError.new("edge v us out of range") unless exists?(v)
+    @adj[u] << v
+    @adj[v] << u
+  end
+
+  # Counts vertices in each component.
+  def component_orders
+    vis = BitArray.new(@adj.size)
+    (0...@adj.size).each.map { |start| dfs(vis, start) }.reject(&.zero?)
+  end
+
+  private def dfs(vis, src)
+    return 0 if vis[src]
+    vis[src] = true
+    1 + @adj[src].sum { |dest| dfs(vis, dest) }
+  end
+
+  private def exists?(vertex)
+    0 <= vertex < @adj.size
+  end
+end
+
+def read_record
+  gets.as(String).split.map(&.to_i)
+end
+
+def read_graph
+  vertex_count, edge_count = read_record
+  graph = Graph.new(vertex_count)
+
+  edge_count.times do
+    u, v = read_record
+    graph.add_edge(u, v)
+  end
+
+  graph
+end
+
+def count_pairs(cardinalities)
+  singles = pairs = 0i64
+
+  cardinalities.each do |cardinality|
+    pairs += singles * cardinality
+    singles += cardinality
+  end
+
+  pairs
+end
+
+puts count_pairs(read_graph.component_orders)
