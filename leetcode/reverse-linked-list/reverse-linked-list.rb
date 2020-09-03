b274347494f9e70075035917924e680c9ac960aa
Eliah Kagan <degeneracypressure@gmail.com>
2020-09-03 10:00:18 -0400

    LeetCode - Reverse Linked List, passed

---
+++ leetcode/reverse-linked-list/reverse-linked-list.rb
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/reverse-linked-list
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+  acc = nil
+
+  while head
+    nxt = head.next
+    head.next = acc
+    acc = head
+    head = nxt
+  end
+
+  acc
+end
