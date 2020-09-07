5c1c51937770b3c60cff366ae8c61b15c6306c59
Eliah Kagan <degeneracypressure@gmail.com>
2020-09-07 19:03:11 -0400

    tree-based implementation of Median Updates
    The heap-based implementation worked, and the optimized version,
    median-updates-fast.cs, passed all the test cases. (The unoptimized
    version, median-updates.cs, passed all but one test case, which
    timed out. Running that test locally did produce the correct
    result.)
    
    But this approach is simpler, and given the complexity of the heap-
    based way (with two layers of hash lookups), this should be faster.
    
    The usual tree-based approaches to Median Updates uses one or two
    multisets. Some languages' standard libraries, like that of C++,
    have such a data structure. Or one may implement it from scratch.
    (Or, outside the setting of "competitive programming," use an
    implementation from a high-quality third-party library.)
    
    Equipped with tree-based multisets, one may use a two-tree approach
    analogous to the two-heap approach typically used to solve the
    easier problem where values are only ever added, never removed.
    
    Or, if one can step back and forth in the tree, one can use a
    single tree and keep track of the medial node(s) in the order they
    would be reached in inorder traversal.
    
    Or, if one has an order statistic tree, one may use that.
    
    But there are two other ways to implement a tree-based multiset.
    Many frameworks provide a set but not a bag/multiset. .NET/Mono is
    such a framework. (There is a ConcurrentBag class, but it is ill
    suited for this use.)
    
    One is to attach a unique identifier to each element in the tree,
    to break ties and allow duplciates to effectively be stored. I've
    done that before (though I haven't redone it in this repository).
    
    The other is to use the tree set to store the frequency of each
    element. The cardinality should also be manually tracked, so it can
    be looked up in O(1) -- since the underlying tree set type tracks
    how many unique elements there are, rather than how many elements.
    
    I'm not sure I've actually implemented it that way before, even
    though it is conceptually (and, I believe, practically) quite
    simple. That is the approach I've taken here.
    
    Currently, this is only very minimally tested.

---
+++ hackerrank/data-structures/balanced-trees/median-updates/median-updates-tree.cs
@@ -0,0 +1,185 @@
+// HackerRank - Median Updates
+// https://www.hackerrank.com/challenges/median
+// In C# 6.0, because that's what HackerRank supports.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal static class ReverseComparer {
+    internal static IComparer<T> Reverse<T>(this IComparer<T> comparer)
+        => new ReverseComparer<T>(comparer);
+}
+
+internal sealed class ReverseComparer<T> : IComparer<T> {
+    internal ReverseComparer(IComparer<T> comparer) => _comparer = comparer;
+
+    public int Compare(T lhs, T rhs) => _comparer.Compare(rhs, lhs);
+
+    private readonly IComparer<T> _comparer;
+}
+
+internal static class SortedBag {
+    internal static SortedBag<T> CreateMinTree<T>()
+        => new SortedBag<T>(Comparer<T>.Default);
+
+    internal static SortedBag<T> CreateMaxTree<T>()
+        => new SortedBag<T>(Comparer<T>.Default.Reverse());
+}
+
+internal sealed class SortedBag<T> {
+
+    internal SortedBag(IComparer<T> comparer)
+        => _freqs = new SortedDictionary<T, int>(comparer);
+
+    internal int Count { get; private set; } = 0;
+
+    internal void Push(T value)
+    {
+        if (_freqs.TryGetValue(value, out var freq))
+            _freqs[value] = freq + 1;
+        else
+            _freqs.Add(value, 1);
+
+        ++Count;
+    }
+
+    internal bool Remove(T value)
+    {
+        if (!_freqs.TryGetValue(value, out var freq)) return false;
+
+        if (freq == 1)
+            _freqs.Remove(value);
+        else
+            _freqs[value] = freq - 1;
+
+        --Count;
+        return true;
+    }
+
+    internal T Pop()
+    {
+        var entry = _freqs.First();
+        var value = entry.Key;
+        var freq = entry.Value;
+
+        if (freq == 1)
+            _freqs.Remove(value);
+        else
+            _freqs[value] = freq - 1;
+
+        --Count;
+        return value;
+    }
+
+    internal T Peek() => _freqs.First().Key;
+
+    private readonly SortedDictionary<T, int> _freqs;
+}
+
+internal sealed class MedianBag {
+    internal int Count => _low.Count + _high.Count;
+
+    internal void Add(int value)
+    {
+        if (_low.Count != 0 && value < _low.Peek())
+            _low.Push(value);
+        else
+            _high.Push(value);
+
+        Rebalance();
+    }
+
+    internal bool Remove(int value)
+    {
+        if (_low.Remove(value) || _high.Remove(value)) {
+            Rebalance();
+            return true;
+        }
+
+        return false;
+    }
+
+    internal double Median
+    {
+        get {
+            if (Count == 0)
+                throw new InvalidOperationException("empty bag has no median");
+
+            switch (BalanceFactor) {
+            case -1:
+                return _low.Peek();
+
+            case +1:
+                return _high.Peek();
+
+            case 0:
+                return ((double)_low.Peek() + (double)_high.Peek()) / 2.0;
+
+            default:
+                throw new NotSupportedException(
+                        "Bug: balancing invariant violated");
+            }
+        }
+    }
+
+    private void Rebalance()
+    {
+        switch (BalanceFactor) {
+        case -2:
+            _high.Push(_low.Pop());
+            break;
+
+        case +2:
+            _low.Push(_high.Pop());
+            break;
+
+        default:
+            break; // Can't be made more balanced.
+        }
+    }
+
+    private int BalanceFactor => _high.Count - _low.Count;
+
+    private readonly SortedBag<int> _low = SortedBag.CreateMaxTree<int>();
+
+    private readonly SortedBag<int> _high = SortedBag.CreateMinTree<int>();
+}
+
+internal static class Solution {
+    private static int ReadValue() => int.Parse(Console.ReadLine());
+
+    private static string[] ReadTokens()
+        => Console.ReadLine()
+                  .Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+
+    private static void Main()
+    {
+        var bag = new MedianBag();
+
+        for (var count = ReadValue(); count > 0; --count) {
+            var tokens = ReadTokens();
+            var opcode = tokens[0];
+            var argument = int.Parse(tokens[1]);
+
+            switch (opcode) {
+            case "a":
+                bag.Add(argument);
+                Console.WriteLine(bag.Median);
+                break;
+
+            case "r":
+                if (bag.Remove(argument) && bag.Count != 0)
+                    Console.WriteLine(bag.Median);
+                else
+                    Console.WriteLine("Wrong!");
+
+                break;
+
+            default:
+                throw new InvalidOperationException(
+                        $"unrecognized opcode \"{opcode}\"");
+            }
+        }
+    }
+}
