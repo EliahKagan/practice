53f92b23eacbfb9d9572e979a35e9f4da74d18c5
Eliah Kagan <degeneracypressure@gmail.com>
2020-09-07 06:13:36 -0400

    scratchwork sort of showing what is happening
    Values added to heaps, starting with the value 4 in the example
    shown in the scratchwork added here, just disappear, being replaced
    by other values.
    
    I think the problem may have to do with an assignment of the form
    (x, y) = (y, x). Is mcs implementing an old C# dialect that doesn't
    have ValueTuple support and, if so, is this involving the comma
    operator?
    
    I would expect that sort of thing, in C#, to give a compile error,
    or at least a warning. But perhaps not.

---
+++ hackerrank/data-structures/heap/find-the-running-median/find-the-running-median-scratch.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal sealed class ReverseComparer<T> : IComparer<T> {
+    internal ReverseComparer(IComparer<T> comparer) => _comparer = comparer;
+
+    public int Compare(T lhs, T rhs) => _comparer.Compare(rhs, lhs);
+
+    private readonly IComparer<T> _comparer;
+}
+
+internal sealed class PriorityQueue<T> {
+    internal static PriorityQueue<T> CreateMinHeap()
+        => new PriorityQueue<T>(Comparer<T>.Default);
+
+    internal static PriorityQueue<T> CreateMaxHeap()
+        => new PriorityQueue<T>(new ReverseComparer<T>(Comparer<T>.Default));
+
+    internal PriorityQueue(IComparer<T> comparer) => _comparer = comparer;
+
+    internal int Count => _heap.Count;
+
+    internal void Push(T value)
+    {
+        _heap.Add(value);
+        SiftUp(Count - 1);
+    }
+
+    internal T Pop()
+    {
+        var value = Peek();
+        _heap[0] = _heap[Count - 1];
+        _heap.RemoveAt(Count - 1);
+        SiftDown(0);
+        return value;
+    }
+
+    internal T Peek()
+    {
+        if (Count == 0) {
+            throw new InvalidOperationException(
+                    "empty heap has no first element");
+        }
+
+        return _heap[0];
+    }
+
+    internal void Reveal(string label)
+    {
+        Console.Write($"{label}:");
+        
+        foreach (var i in Enumerable.Range(0, Count))
+            Console.Write($" [{i}]={_heap[i]}");
+
+        Console.WriteLine();
+    }
+
+    private const int NoChild = -1;
+
+    private void SiftUp(int child)
+    {
+        while (child != 0) {
+            var parent = (child - 1) / 2;
+            if (OrderOk(parent, child)) break;
+
+            Swap(parent, child);
+            child = parent;
+        }
+    }
+
+    private void SiftDown(int parent)
+    {
+        for (; ; ) {
+            var child = PickChild(parent);
+            if (child == NoChild || OrderOk(parent, child)) break;
+
+            Swap(parent, child);
+            parent = child;
+        }
+    }
+
+    private int PickChild(int parent)
+    {
+        var left = parent * 2 + 1;
+        if (left >= Count) return NoChild;
+
+        var right = left + 1;
+        return right == Count || OrderOk(left, right) ? left : right;
+    }
+
+    private bool OrderOk(int parent, int child)
+        => _comparer.Compare(_heap[parent], _heap[child]) <= 0;
+
+    private void Swap(int i, int j)
+        => (_heap[i], _heap[j]) = (_heap[j], _heap[i]);
+
+    private readonly IList<T> _heap = new List<T>();
+
+    private readonly IComparer<T> _comparer;
+}
+
+internal sealed class MedianBag {
+    internal void Add(int value)
+    {
+        if (_low.Count != 0 && value < _low.Peek())
+            _low.Push(value);
+        else
+            _high.Push(value);
+
+        Rebalance();
+    }
+
+    internal double Median
+    {
+        get {
+            switch (BalanceFactor) {
+            case -1:
+                return _low.Peek();
+
+            case +1:
+                return _high.Peek();
+
+            case 0:
+                return (_low.Peek() + _high.Peek()) / 2.0;
+
+            default:
+                throw new NotSupportedException(
+                        "Bug: balancing invariant violated");
+            }
+        }
+    }
+
+    private void Rebalance()
+    {
+        switch (BalanceFactor) {
+        case -2:
+            _high.Push(_low.Pop());
+            break;
+
+        case +2:
+            _low.Push(_high.Pop());
+            break;
+
+        default:
+            break; // Can't be made more balanced.
+        }
+
+        _low.Reveal("_low");
+        _high.Reveal("_high");
+    }
+
+    private int BalanceFactor => _high.Count - _low.Count;
+
+    private readonly PriorityQueue<int> _low =
+        PriorityQueue<int>.CreateMaxHeap();
+
+    private readonly PriorityQueue<int> _high =
+        PriorityQueue<int>.CreateMinHeap();
+}
+
+internal static class Solution {
+    private static int ReadValue() => int.Parse(Console.ReadLine());
+
+    private static void Main()
+    {
+        var bag = new MedianBag();
+
+        foreach (var value in Enumerable.Range(1, 10)) {
+            Console.WriteLine();
+            bag.Add(value);
+            Console.WriteLine($"Median after {value}:  {bag.Median:F1}");
+        }
+    }
+}
