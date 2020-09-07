575be15327ab257f29751adc25421fd82306d270
Eliah Kagan <degeneracypressure@gmail.com>
2020-09-07 06:51:30 -0400

    here's an MCVE

--- hackerrank/data-structures/heap/find-the-running-median/scratch.cs
+++ hackerrank/data-structures/heap/find-the-running-median/scratch.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections.Generic;
 
-internal sealed class PriorityQueue<T> {
-    internal void Swap(int i, int j)
-        => (_heap[i], _heap[j]) = (_heap[j], _heap[i]);
+internal sealed class Pair {
+    public override string ToString() => $"({_items[0]}, {_items[1]})";
 
-    private readonly IList<T> _heap = new List<T>();
+    internal void Swap(int i, int j)
+        => (_items[i], _items[j]) = (_items[j], _items[i]);
+
+    private readonly IList<string> _items = new List<string>() { "foo", "bar" };
 }
 
 internal static class Program {
     private static void Main()
     {
-        var pq = new PriorityQueue<int>();
+        var pair = new Pair();
+        Console.WriteLine(pair);
+        pair.Swap(0, 1);
+        Console.WriteLine(pair);
     }
 }
