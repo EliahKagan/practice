85841bb2d6ab5f4942d9c50618622ddd7b5548b9
Eliah Kagan <degeneracypressure@gmail.com>
2020-09-07 09:16:40 -0400

    simpler MCVE
    This looks like a bug in the Mono compiler.

--- hackerrank/data-structures/heap/find-the-running-median/scratch.cs
+++ hackerrank/data-structures/heap/find-the-running-median/scratch.cs
@@ -1,21 +1,14 @@
 using System;
 using System.Collections.Generic;
 
-internal sealed class Pair {
-    public override string ToString() => $"({_items[0]}, {_items[1]})";
-
-    internal void Swap(int i, int j)
-        => (_items[i], _items[j]) = (_items[j], _items[i]);
-
-    private readonly IList<string> _items = new List<string>() { "foo", "bar" };
-}
-
 internal static class Program {
     private static void Main()
     {
-        var pair = new Pair();
-        Console.WriteLine(pair);
-        pair.Swap(0, 1);
-        Console.WriteLine(pair);
+        var a = new List<string> { "foo", "bar" };
+        var i = 0;
+        var j = 1;
+        Console.WriteLine(string.Join(" ", a));
+        (a[i], a[j]) = (a[j], a[i]);
+        Console.WriteLine(string.Join(" ", a));
     }
 }
