8e2ff658e9f41ddff8ef5399784834915b09c6cc
Eliah Kagan <degeneracypressure@gmail.com>
2020-10-18 15:36:47 -0400

    also strip tags and consolidate whitespace (passes)

--- hackerrank/regex/applications/detect-html-links.pl
+++ hackerrank/regex/applications/detect-html-links.pl
@@ -5,12 +5,24 @@ use warnings;
 use 5.022;
 use English;
 
+sub strip_tags {
+    my ($text) = @_;
+    $text =~ s/<.*?>//msxg;
+    return $text;
+}
+
+sub consolidate_whitespace {
+    my ($text) = @_;
+    return join q{ }, $text =~ /\S+/msxg;
+}
+
 local $INPUT_RECORD_SEPARATOR = undef;
 my $corpus = <<>>;
 my @links = $corpus =~ m{<a\s(?:.*?\s)??href="(.+?)".*?>(.*?)</a>}msxig;
 
 while (@links) {
     my $url = shift @links;
-    my $text = shift @links;
+    my $raw_text = shift @links;
+    my $text = consolidate_whitespace(strip_tags($raw_text));
     print "$url,$text\n";
 }
