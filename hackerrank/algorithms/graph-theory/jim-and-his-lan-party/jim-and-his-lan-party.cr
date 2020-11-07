81be4027f630cd2473babbf738f1ee5597ded691
Eliah Kagan <degeneracypressure@gmail.com>
2020-11-06 20:03:18 -0500

    added comments; better name for never-connected value

--- hackerrank/algorithms/graph-theory/jim-and-his-lan-party/jim-and-his-lan-party.cr
+++ hackerrank/algorithms/graph-theory/jim-and-his-lan-party/jim-and-his-lan-party.cr
@@ -1,9 +1,14 @@
 # Jim and his LAN Party
 # https://www.hackerrank.com/challenges/jim-and-his-lan-party
 
-INCOMPLETE = -1
+NOT_CONNECTED = -1
 
+# A network of elements representing users or machines, each of whom is a
+# member of exactly one group, which can meet/play when all members are
+# connected in a component (possibly shared with members of other groups).
 class Network
+  # Creates a new network with groups in [0, group_count) and elements in
+  # [0, element_groups.size). Element i is a member of element_groups[i].
   def initialize(group_count : Int32, element_groups)
     element_groups = element_groups.to_a
     total_size = element_groups.size
@@ -14,21 +19,24 @@ class Network
     element_groups.each { |group| @group_sizes[group] += 1 }
 
     @group_completion_times = @group_sizes.map do |size|
-      size < 2 ? @time : INCOMPLETE
+      size < 2 ? @time : NOT_CONNECTED
     end
 
     @elem_contributions = element_groups.map_with_index do |group, elem|
-      @group_completion_times[group] == INCOMPLETE ? {group => 1} : nil
+      @group_completion_times[group] == NOT_CONNECTED ? {group => 1} : nil
     end
 
     @elem_parents = Array(Int32).new(total_size) { |elem| elem }
     @elem_ranks = Array(Int32).new(total_size, 0)
   end
 
+  # Retrieves the times at which each group finished becoming connected.
+  # For groups that were never connected, NOT_CONNECTED is returned.
   def completion_times
     @group_completion_times.each
   end
 
+  # Adds an edge/wire connecting elem1 and elem2.
   def connect(elem1, elem2)
     @time += 1
 
@@ -37,7 +45,7 @@ class Network
     elem2 = find_set(elem2)
     return if elem1 == elem2
 
-    # Unite by rank, merging contributions.
+    # Union by rank, merging contributions.
     if @elem_ranks[elem1] < @elem_ranks[elem2]
       join(parent: elem2, child: elem1)
     else
@@ -46,6 +54,7 @@ class Network
     end
   end
 
+  # Union-find "findset" operation with full path compression.
   private def find_set(elem)
     # Find the ancestor.
     leader = elem
@@ -63,6 +72,7 @@ class Network
     leader
   end
 
+  # Makes child a child of parent and merges their contributions into parent.
   private def join(parent, child)
     @elem_parents[child] = parent
 
@@ -74,6 +84,9 @@ class Network
     nil
   end
 
+  # Merges contributions into whichever contribution hash started out larger,
+  # recording an removing items that become complete as a result.
+  # Returns the resulting hash, or nil if that hash is ultimately empty.
   private def merge_contributions(contrib1, contrib2)
     # If either are both are nil, there is nothing to do.
     return contrib1 || contrib2 unless contrib1 && contrib2
@@ -86,6 +99,7 @@ class Network
     end
   end
 
+  # Merges the source hash into the sink hash. Helper for merge_contributions.
   private def do_merge_contributions(sink, source)
     source.each do |group, source_count|
       sink_count = sink[group]?
@@ -111,17 +125,22 @@ class Network
   @elem_ranks : Array(Int32)
 end
 
+# Reads a line as a sequence of integers.
 def read_record
   gets.as(String).split.map(&.to_i)
 end
 
+# Read the problem parameters. (player_count is currently not checked.)
 player_count, game_count, wire_count = read_record
 
+# Make the LAN. The extra 0 element and 0 group facilitate 1-based indexing.
 network = Network.new(game_count + 1, read_record.unshift(0))
 
+# Read and apply all the connections.
 wire_count.times do
   elem1, elem2 = read_record
   network.connect(elem1, elem2)
 end
 
+# Skip the extra 0 group and print the other groups' completion times.
 network.completion_times.skip(1).each { |time| puts time }
