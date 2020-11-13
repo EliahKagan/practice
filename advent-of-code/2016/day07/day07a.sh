5e553d82a24e485f581425a3831528dcdf9e783b
Eliah Kagan <degeneracypressure@gmail.com>
2020-11-13 14:42:48 -0500

    improved regex readability for part A
    This regex is not actually equivalent, but it works just as well,
    and in the same way. It matches word characters instead of
    characters other than specific punctuation. Since all the non "["
    and "]" characters in the input are word characters, this works as
    well and is far more readable (due to "\w").
    
    The corresponding readability approach is already taken in my
    solution to part B (day07b.pl).

--- advent-of-code/2016/day07/day07a.sh
+++ advent-of-code/2016/day07/day07a.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # Advent of Code 2016, day 7, part A
 
-grep -P '([^][])(?!\1)([^][])\2\1' input |
-    grep -cvP '\[[^]]*([^][])(?!\1)([^][])\2\1'
+grep -P '(\w)(?!\1)(\w)\2\1' "$@" | grep -cvP '\[\w*(\w)(?!\1)(\w)\2\1'
