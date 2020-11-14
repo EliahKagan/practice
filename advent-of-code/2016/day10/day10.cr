fa88cfa20933af5e364c1ecaaecd0ee438377a8d
Eliah Kagan <degeneracypressure@gmail.com>
2020-11-14 17:26:17 -0500

    started on day 10

---
+++ advent-of-code/2016/day10/day10.cr
@@ -0,0 +1,40 @@
+# Advent of Code 2016, day 10, part A
+
+class BotCircus
+  class Error < Exception
+  end
+
+  private struct Bot
+    def subscribe(&action : Proc(Int32, Int32, Nil))
+      raise Error.new("can't subscribe two event handlers") if @action
+      @action = action
+      maybe_run
+    end
+
+    def put(value : Int32)
+      low = @low
+      if low.nil?
+        @low = value
+      elsif @high.nil
+        @low, @high = {low, value}.minmax
+        maybe_run
+      else
+        raise Error.new("bot can't take a third value")
+      end
+    end
+
+    private def maybe_run
+      action = @action
+      low = @low
+      high = @high
+      return unless action && low && high
+
+      @low = @high = nil
+      action.call(low, high)
+    end
+
+    @action : Proc(Int32, Int32, Nil)? = nil
+    @low : Int32? = nil
+    @high : Int32? = nil
+  end
+end
