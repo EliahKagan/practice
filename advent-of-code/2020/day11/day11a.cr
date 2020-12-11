be8f115a6e8de095ce7ad28577afb03eef4e7610
Eliah Kagan <degeneracypressure@gmail.com>
2020-12-11 06:25:32 -0500

    Advent of Code 2020, day 11, part A

---
+++ advent-of-code/2020/day11/day11a.cr
@@ -0,0 +1,127 @@
+# Advent of Code 2020, day 11, part A
+
+require "option_parser"
+
+class Board
+  class Error < Exception
+  end
+
+  def initialize(io : IO)
+    lines = io.each_line.map(&.rstrip).to_a
+    while !lines.empty? && lines.last.empty?
+      lines.pop
+    end
+    raise Error.new("empty board not supported") if lines.empty?
+
+    if lines.any? { |line| line.size != lines.first.size }
+      raise Error.new("jagged board not supported")
+    end
+
+    if lines.first.empty?
+      raise Error.new("zero-width board not supported")
+    end
+
+    @rows = lines.map(&.chars)
+
+    0.upto(height - 1) do |i|
+      0.upto(width - 1) do |j|
+        unless ".L#".includes?(@rows[i][j])
+          raise Error.new("invalid character #{@rows[i][j]} at (#{i}, #{j})")
+        end
+      end
+    end
+  end
+
+  def ==(other : Board)
+    @rows == other.rows
+  end
+
+  def to_s(io)
+    @rows.each { |row| io << row.join << '\n' }
+    io
+  end
+
+  def height
+    @rows.size
+  end
+
+  def width
+    @rows.first.size
+  end
+
+  def successor
+    Board.new(self, EvolveTag.new)
+  end
+
+  def count(ch : Char)
+    @rows.sum(&.count(ch))
+  end
+
+  private struct EvolveTag
+  end
+
+  protected def initialize(original : Board, tag : EvolveTag)
+    @rows = original.rows.map(&.dup)
+
+    0.upto(height - 1) do |i|
+      0.upto(width - 1) do |j|
+        case original.rows[i][j]
+        when 'L'
+          @rows[i][j] = '#' if original.count_adjacent(i, j, '#').zero?
+        when '#'
+          @rows[i][j] = 'L' if original.count_adjacent(i, j, '#') >= 4
+        when '.'
+          next # Spaces with no seat are never changed.
+        else
+          raise "Bug: invalid cell state"
+        end
+      end
+    end
+  end
+
+  protected getter rows : Array(Array(Char))
+
+  protected def count_adjacent(i, j, ch)
+    count = 0
+
+    (i - 1).upto(i + 1) do |h|
+      (j - 1).upto(j + 1) do |k|
+        count += 1 if has_pos?(h, k) && {h, k} != {i, j} && @rows[h][k] == ch
+      end
+    end
+
+    count
+  end
+
+  private def has_pos?(i, j)
+    0 <= i < height && 0 <= j < width
+  end
+end
+
+verbosity = 0
+
+OptionParser.parse do |parser|
+  parser.on "-v", "--verbose",
+            "Verbose output (pass multiple times for more verbosity)" do
+    verbosity += 1
+  end
+  parser.on "-h", "--help", "Show options help" do
+    puts parser
+    exit
+  end
+end
+
+if verbosity == 1
+  STDERR.puts %Q[#{PROGRAM_NAME}: for more verbosity, pass "-v" twice ("-v -v")]
+end
+
+board = Board.new(ARGF)
+loop do
+  evolved = board.successor
+  break if evolved == board
+  puts board if verbosity >= 2
+  board = evolved
+end
+
+puts board if verbosity >= 1
+puts board.count('#')
