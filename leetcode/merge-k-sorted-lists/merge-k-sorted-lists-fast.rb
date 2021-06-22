b3f4294b72278d14021597e3cd0bdc9ccb188d78
Eliah Kagan <degeneracypressure@gmail.com>
2021-06-22 07:09:09 -0400

    faster Merge k Sorted Lists implementation (custom priority queue)
    In merge-k-sorted-lists.rb, I used a slightly modified version of
    the general purpose priority queue I wrote for my Ruby solution to
    "Find the Running Median" (on HackerRank).
    
    In merge-k-sorted-lists-fast.rb, I wrote the priority queue
    specifically to compare ListNode objects by calling their val
    methods. Though also do fewer writes (rather than swapping), which
    may be the more important difference.
    
    Both use binary heap priority queues.

---
+++ leetcode/merge-k-sorted-lists/merge-k-sorted-lists-fast.rb
@@ -0,0 +1,93 @@
+# LeetCode #23 - Merge k Sorted Lists
+# https://leetcode.com/problems/merge-k-sorted-lists/
+# Using a custom binary minheap priority queue.
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode[]} lists
+# @return {ListNode}
+def merge_k_lists(lists)
+  pre = sentinel = ListNode.new
+
+  heap = MinNodeHeap.new
+  lists.each { |head| heap.push(head) if head }
+
+  until heap.empty?
+    head = heap.pop
+    pre = pre.next = head
+    heap.push(head.next) if head.next
+  end
+
+  pre.next = nil
+  sentinel.next
+end
+
+# A binary-heap min priority queue comparing list nodes by value.
+class MinNodeHeap
+  def initialize
+    @heap = []
+  end
+
+  def empty?
+    @heap.empty?
+  end
+
+  def push(node)
+    sift_up(@heap.size, node)
+  end
+
+  def pop
+    case @heap.size
+    when 0
+      raise "can't pop from empty heap"
+    when 1
+      @heap.pop
+    else
+      ret = @heap[0]
+      sift_down(0, @heap.pop)
+      ret
+    end
+  end
+
+  private
+
+  def sift_up(child, child_node)
+    until child.zero?
+      parent = (child - 1) / 2
+      break unless child_node.val < @heap[parent].val
+
+      @heap[child] = @heap[parent]
+      child = parent
+    end
+
+    @heap[child] = child_node
+    nil
+  end
+
+  def sift_down(parent, parent_node)
+    loop do
+      child = pick_child(parent)
+      break unless child && @heap[child].val < parent_node.val
+
+      @heap[parent] = @heap[child]
+      parent = child
+    end
+
+    @heap[parent] = parent_node
+    nil
+  end
+
+  def pick_child(parent)
+    left = parent * 2 + 1
+    return nil if left >= @heap.size
+
+    right = left + 1
+    right != @heap.size && @heap[right].val < @heap[left].val ? right : left
+  end
+end
