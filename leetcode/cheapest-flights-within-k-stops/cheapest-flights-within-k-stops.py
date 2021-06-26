87c7f4446f10e5d85600506d7b8c466da7134374
Eliah Kagan <degeneracypressure@gmail.com>
2021-06-26 11:19:28 -0400

    in Python, has bug
    The bug appears to be due to how I lazily (and not in the good way)
    wrote the loop that adds the edges to the graph in the top-level
    function and used, as one of the loop variables, the same variable
    name as in one of the function parameters.
    
    (Even if somehow this is not the cause of the bug--which is very
    unlikely--it is in and of itself a bug and must be fixed. The
    variable name could be changed, but it is better to extract the
    code that builds the graph to a separate function... as I nearly
    always do, but for whatever reason laxly didn't bother with this
    time.)

---
+++ leetcode/cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -0,0 +1,65 @@
+# LeetCode #787 - Cheapest Flights Within K Stops
+# https://leetcode.com/problems/cheapest-flights-within-k-stops/
+# Via BFS with relaxations (compare to Dijkstra's algorithm).
+
+class Solution:
+    @staticmethod
+    def findCheapestPrice(n: int,
+                          flights: List[List[int]],
+                          src: int,
+                          dst: int,
+                          k: int) -> int:
+        graph = Graph(n)
+        for src, dest, weight in flights:
+            graph.add_edge(src, dest, weight)
+
+        min_cost = graph.min_path_cost(src, dst, k + 1)
+        return -1 if min_cost == math.inf else min_cost
+
+
+class Graph:
+    """A weighted directed graph."""
+
+    __slots__ = ('_adj',)
+
+    def __init__(self, vertex_count):
+        self._adj = [[] for _ in range(vertex_count)]
+
+    def add_edge(self, src, dest, weight):
+        self._check(src)
+        self._check(dest)
+        self._adj[src].append((dest, weight))
+
+    def min_path_cost(self, start, finish, max_depth):
+        self._check(start)
+        self._check(finish)
+
+        costs = [math.inf] * self._vertex_count
+        queue = collections.deque()
+        costs[start] = 0
+        queue.append((start, 0))
+
+        for _ in range(max_depth):
+            if not queue:
+                break
+
+            for _ in range(len(queue)):
+                src, src_cost = queue.popleft()
+
+                for dest, weight in self._adj[src]:
+                    dest_cost = src_cost + weight
+                    if costs[dest] <= dest_cost:
+                        continue
+
+                    costs[dest] = dest_cost
+                    queue.append((dest, dest_cost))
+
+        return costs[finish]
+
+    @property
+    def _vertex_count(self):
+        return len(self._adj)
+
+    def _check(self, vertex):
+        if not 0 <= vertex < self._vertex_count:
+            raise ValueError('vertex out of range')
