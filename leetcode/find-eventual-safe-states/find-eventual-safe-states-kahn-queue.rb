877f1702a6cba2914c18171f1dbb6b9cb5d9e533
Eliah Kagan <degeneracypressure@gmail.com>
2021-06-27 13:07:13 -0400

    Find Eventual Safe States - fixed Kahn's algorithm
    I tranposed the graph, as I forgot to do before. This passes.

--- leetcode/find-eventual-safe-states/find-eventual-safe-states-kahn-queue.rb
+++ leetcode/find-eventual-safe-states/find-eventual-safe-states-kahn-queue.rb
@@ -1,11 +1,12 @@
 # LeetCode #802 - Find Eventual Safe States
 # https://leetcode.com/problems/find-eventual-safe-states/
-# By Kahn's algorithm with a queue.
+# By Kahn's algorithm (with a queue) on the transpose graph.
 
 # @param {Integer[][]} graph
 # @return {Integer[]}
 def eventual_safe_nodes(graph)
-  indegrees = compute_indegrees(graph)
+  indegrees = graph.map(&:size)
+  graph = transpose_graph(graph)
   roots = indegrees.indices(&:zero?)
 
   until roots.empty?
@@ -17,10 +18,14 @@ def eventual_safe_nodes(graph)
   indegrees.indices(&:zero?)
 end
 
-def compute_indegrees(graph)
-  indegrees = [0] * graph.size
-  graph.each { |row| row.each { |dest| indegrees[dest] += 1 } }
-  indegrees
+def transpose_graph(graph)
+  tr_graph = Array.new(graph.size) { [] }
+
+  graph.each_with_index do |row, src|
+    row.each { |dest| tr_graph[dest] << src }
+  end
+
+  tr_graph
 end
 
 # Convenicence extensions for arrays.
