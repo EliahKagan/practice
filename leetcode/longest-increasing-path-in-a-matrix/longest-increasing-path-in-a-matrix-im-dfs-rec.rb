0af9ebd8c6b7c9ad1eab07b7c30aac759077eb68
Eliah Kagan <degeneracypressure@gmail.com>
2021-07-06 19:08:42 -0400

    LeetCode: Longest Increasing Path in a Matrix, recursive DFS
    In Ruby. On an implicit graph. Similar to DFS toposort. Passed.

---
+++ leetcode/longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix-im-dfs-rec.rb
@@ -0,0 +1,26 @@
+# LeetCode #329 - Longest Increasing Path in a Matrix
+# https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
+# By memoized recursive implicit-graph postorder DFS, similar to DFS toposort.
+
+# @param {Integer[][]} matrix
+# @return {Integer}
+def longest_increasing_path(matrix)
+  height = matrix.size
+  width = matrix.first.size
+
+  neighbors = lambda do |i, j|
+    [[i, j - 1], [i, j + 1], [i - 1, j], [i + 1, j]].filter do |h, k|
+      h.between?(0, height - 1) && k.between?(0, width - 1) &&
+          matrix[i][j] < matrix[h][k]
+    end
+  end
+
+  dp = {} # maximum forward path lengths
+
+  dfs = lambda do |i, j|
+    dp[[i, j]] ||=
+      (neighbors.call(i, j).map { |h, k| dfs.call(h, k) }.max || 0) + 1
+  end
+
+  (0...height).map { |i| (0...width).map { |j| dfs.call(i, j) }.max }.max
+end
