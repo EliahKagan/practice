55c95b7a62f3f911a109a43222d906bcb2374602
Eliah Kagan <degeneracypressure@gmail.com>
2021-07-07 18:56:35 -0400

    LeetCode: Reconstruct Itinerary, recursive Hierholzer
    In Ruby. Building a doubly linked list. Passed.

---
+++ leetcode/reconstruct-itinerary/reconstruct-itinerary-rec-dll.rb
@@ -0,0 +1,67 @@
+# LeetCode #332 - Reconstruct Itinerary
+# https://leetcode.com/problems/reconstruct-itinerary/
+# By Hierholzer's algorithm, recursively building a doubly linked list.
+
+# @param {String[][]} tickets
+# @return {String[]}
+def find_itinerary(tickets)
+  adj = build_adjacency_list(tickets)
+  start = Node.new(nil, 'JFK')
+
+  dfs = lambda do |node|
+    neighbors = adj[node.value]
+    next unless neighbors
+
+    dfs.call(Node.new(node, neighbors.shift)) until neighbors.empty?
+  end
+
+  dfs.call(start)
+  start.to_a
+end
+
+def build_adjacency_list(tickets)
+  adj = {}
+  tickets.each { |src, dest| (adj[src] ||= []) << dest }
+  adj.each_value(&:sort!)
+  adj
+end
+
+# A doubly linked list node.
+class Node
+  attr_reader :prev, :next, :value
+
+  def initialize(prev, value)
+    if prev
+      @prev = prev
+      @next = prev.next
+
+      @prev.next = self
+      @next.prev = self if @next
+    else
+      @prev = @next = nil
+    end
+
+    @value = value
+  end
+
+  def to_a
+    values = []
+    each_from_here_forward { |value| values << value }
+    values
+  end
+
+  protected
+
+  attr_writer :prev, :next
+
+  private
+
+  def each_from_here_forward
+    node = self
+
+    while node
+      yield node.value
+      node = node.next
+    end
+  end
+end
