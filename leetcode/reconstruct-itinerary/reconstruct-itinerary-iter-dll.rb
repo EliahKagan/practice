c1330472b1cb238850720e27620ff616b95b00da
Eliah Kagan <degeneracypressure@gmail.com>
2021-07-07 19:27:18 -0400

    added iterative version using doubly linked list

---
+++ leetcode/reconstruct-itinerary/reconstruct-itinerary-iter-dll.rb
@@ -0,0 +1,61 @@
+# LeetCode #332 - Reconstruct Itinerary
+# https://leetcode.com/problems/reconstruct-itinerary/
+# By Hierholzer's algorithm, iteratively building a doubly linked list.
+
+# @param {String[][]} tickets
+# @return {String[]}
+def find_itinerary(tickets)
+  adj = build_adjacency_list(tickets)
+  node = start = Node.new(nil, 'JFK')
+
+  while node
+    if (dest = adj[node.value]&.shift)
+      node = Node.new(node, dest)
+    else
+      node = node.prev
+    end
+  end
+
+  start.to_a
+end
+
+def build_adjacency_list(tickets)
+  adj = {}
+  tickets.each { |src, dest| (adj[src] ||= []) << dest }
+  adj.each_value(&:sort!)
+  adj
+end
+
+# A doubly linked list node.
+class Node
+  include Enumerable
+
+  attr_reader :prev, :next, :value
+
+  def initialize(prev, value)
+    if prev
+      @prev = prev
+      @next = prev.next
+
+      @prev.next = self
+      @next.prev = self if @next
+    else
+      @prev = @next = nil
+    end
+
+    @value = value
+  end
+
+  def each
+    node = self
+
+    while node
+      yield node.value
+      node = node.next
+    end
+  end
+
+  protected
+
+  attr_writer :prev, :next
+end
