1c4c9f245f570ad8e3821e8133e7b5771448e6a3
Eliah Kagan <degeneracypressure@gmail.com>
2021-07-21 15:49:26 -0400

    C++ version, incomplete
    This has a bug that sometimes causes it to select extra vertices.
    
    The bug appears to be due to failing to mark the start vertex as
    visited.
    
    (Not yet submitted.)

---
+++ leetcode/get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    [[nodiscard]] static vector<string>
+    watchedVideosByFriends(const vector<vector<string>>& watchedVideos,
+                           const vector<vector<int>>& friends,
+                           int id,
+                           int level) noexcept;
+};
+
+namespace {
+    [[nodiscard]] vector<int> bfs_level(const vector<vector<int>>& adj,
+                                        const int start,
+                                        int level) noexcept
+    {
+        auto vis = vector<char>(size(adj));
+        auto cur = vector{start};
+        auto nxt = vector<int>{};
+
+        while (level-- > 0) {
+            for (const auto src : cur) {
+                for (const auto dest : adj[src]) {
+                    if (vis[dest]) continue;
+
+                    vis[dest] = true;
+                    nxt.push_back(dest);
+                }
+            }
+
+            cur.clear();
+            swap(cur, nxt);
+        }
+
+        return cur;
+    }
+
+    [[nodiscard]] vector<string>
+    concat_indexed_bins(const vector<vector<string>>& bins,
+                        const vector<int>& indices) noexcept
+    {
+        auto out = vector<string>{};
+
+        for (const auto i : indices)
+            copy(cbegin(bins[i]), cend(bins[i]), back_inserter(out));
+
+        return out;
+    }
+
+    [[nodiscard]] vector<string>
+    consolidate_sort(const vector<string>& videos) noexcept
+    {
+        auto out = vector<string>{};
+        auto freqs = unordered_map<string, int>{};
+
+        for (const auto& vid : videos)
+            if (freqs[vid]++ == 0) out.push_back(vid);
+
+        sort(begin(out), end(out), [&freqs](const string& lhs,
+                                            const string& rhs) noexcept {
+            const auto lhs_freq = freqs[lhs];
+            const auto rhs_freq = freqs[rhs];
+            return lhs_freq == rhs_freq ? lhs < rhs : lhs_freq < rhs_freq;
+        });
+
+        return out;
+    }
+}
+
+vector<string>
+Solution::watchedVideosByFriends(const vector<vector<string>>& watchedVideos,
+                                 const vector<vector<int>>& friends,
+                                 const int id,
+                                 const int level) noexcept
+{
+    const auto level_ids = bfs_level(friends, id, level);
+
+    copy(cbegin(level_ids), cend(level_ids), ostream_iterator<int>{cout, "\n"});
+
+    return consolidate_sort(concat_indexed_bins(watchedVideos, level_ids));
+}
