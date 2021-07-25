070d3326d4c2d970a5676c0c80fddb8e496c6554
Eliah Kagan <degeneracypressure@gmail.com>
2021-07-25 11:53:32 -0400

    LeetCode: Number of Closed Islands
    In Ruby. By recursive DFS. Containins some debugging cruft. Passed.

---
+++ leetcode/number-of-closed-islands/number-of-closed-islands-dfs-rec.rb
@@ -0,0 +1,31 @@
+# LeetCde #1254 - Number of Closed Islands
+# https://leetcode.com/problems/number-of-closed-islands/
+# By recursive DFS.
+
+# @param {Integer[][]} grid
+# @return {Integer}
+def closed_island(grid)
+  height = grid.size
+  width = grid.first.size
+
+  dfs = lambda do |i, j|
+    return false unless i.between?(0, height - 1) && j.between?(0, width - 1)
+
+    return true unless grid[i][j].zero?
+
+    grid[i][j] = 2
+
+    dfs.call(i, j - 1) & dfs.call(i, j + 1) &
+        dfs.call(i - 1, j) & dfs.call(i + 1, j)
+  end
+
+  (0...height).sum do |i|
+    (0...width).count do |j|
+      next unless grid[i][j].zero?
+
+      res = dfs.call(i, j)
+      puts "(#{i}, #{j}) -> #{res}"
+      res
+    end
+  end
+end
