5737eb1059b8579e95bb72f3e4ad1e6a35c66d3e
Eliah Kagan <degeneracypressure@gmail.com>
2021-07-27 19:59:29 -0400

    LeetCode: Two Sum IV - Input is a BST, in progress
    This failed when submitted. It has (at least) two bugs:
    
    (1) The BST search is wrong. It goes left when it should go right
        and vice versa. :( :( :(
    
    (2) The problem statement didn't say the "two elements" have to be
        distinct, but this is required.

---
+++ leetcode/2sum-iv-input-is-a-bst/2sum-iv-input-is-a-bst.rb
@@ -0,0 +1,39 @@
+# LeetCode #653 - Two Sum IV - Input is a BST
+# https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+# By repeatedly searching the BST. Runtime is O(size * height).
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} target
+# @return {Boolean}
+def find_target(root, target)
+  each_val(root) { |val| return true if bst_has?(root, target - val) }
+  false
+end
+
+def each_val(root)
+  return unless root
+
+  each_val(root.left) { |val| yield val }
+  yield root.val
+  each_val(root.right) { |val| yield val }
+
+  nil
+end
+
+def bst_has?(root, val)
+  while root
+    return true if root.val == val
+    root = (root.val < val ? root.left : root.right)
+  end
+
+  false
+end
