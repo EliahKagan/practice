e495913448afdd0e170f497746e707e3d3586791
Eliah Kagan <degeneracypressure@gmail.com>
2021-08-08 04:12:59 -0400

    Path With Minimum Effort, bisecting over BFS, has bug
    The bug causs the cost=0 solution on a 1x1 grid not to be found.

---
+++ leetcode/path-with-minimum-effort/path-with-minimum-effort-bfs-bisect.rb
@@ -0,0 +1,92 @@
+# LeetCode #1631 - Path With Minimum Effort
+# https://leetcode.com/problems/path-with-minimum-effort/
+# By bisecting over capped-edge-weight BFS.
+
+require 'set'
+
+# @param {Integer[][]} heights
+# @return {Integer}
+def minimum_effort_path(heights)
+  graph = build_graph(heights)
+  graph.minimize_max_edge_weight(0, graph.order - 1)
+end
+
+def build_graph(heights)
+  m = heights.size
+  n = heights.first.size
+  graph = Graph.new(m * n)
+
+  0.upto(m - 1) do |i|
+    0.upto(n - 1) do |j|
+      if j + 1 != n
+        graph.add_edge(n * i + j,
+                       n * i + j + 1,
+                       (heights[i][j] - heights[i][j + 1]).abs)
+      end
+      if i + 1 != m
+        graph.add_edge(n * i + j,
+                       n * (i + 1) + j,
+                       (heights[i][j] - heights[i + 1][j]).abs)
+      end
+    end
+  end
+
+  graph
+end
+
+# A weighted undirected graph.
+class Graph
+  def initialize(order)
+    @adj = Array.new(order) { [] }
+    @weights = Set.new
+  end
+
+  def order
+    @adj.size
+  end
+
+  def add_edge(u, v, weight)
+    raise 'edge vertex out of range' unless exists?(u) && exists?(v)
+
+    @adj[u] << [v, weight]
+    @adj[v] << [u, weight]
+    @weights << weight
+    nil
+  end
+
+  def minimize_max_edge_weight(start, finish)
+    raise 'vertex out of range' unless exists?(start) && exists?(finish)
+
+    ret = @weights.sort.bsearch { |weight| bfs(start, finish, weight) }
+    raise 'destination vertex not reachable' unless ret
+
+    ret
+  end
+
+  private
+
+  def exists?(vertex)
+    vertex.between?(0, order - 1)
+  end
+
+  def bfs(start, finish, max_weight)
+    return true if start == finish
+
+    vis = [false] * order
+    vis[start] = true
+    queue = [start]
+
+    until queue.empty?
+      @adj[queue.shift].each do |dest, weight|
+        next if vis[dest] || weight > max_weight
+
+        return true if dest == finish
+
+        vis[dest] = true
+        queue << dest
+      end
+    end
+
+    false
+  end
+end
