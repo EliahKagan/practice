618cdf875f295623d79fdf4ec5a7012b4f0005ff
Eliah Kagan <degeneracypressure@gmail.com>
2021-08-20 21:02:56 -0400

    pythonds: with doctest as well

--- ghost.py
+++ ghost.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
 class GhostDict(dict):
-    """A dict that remembers deletions."""
-    
+    """
+    A dict that remembers deletions.
+
+    >>> gd = GhostDict(foo=18)
+    >>> gd['bar'] = 20
+    >>> print(sorted(gd.items()))
+    [('bar', 20), ('foo', 18)]
+
+    >>> del gd['bar']
+    >>> print(gd)
+    {'foo': 18}
+
+    >>> print(gd.recall('bar'))
+    20
+    """
+
     __slots__ = ('_ghost',)
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._ghost = {}
-    
+
     def __delitem__(self, key):
         self._ghost[key] = self[key]
         super().__delitem__(key)
-    
+
     def recall(self, key):
         return self._ghost[key]
 
