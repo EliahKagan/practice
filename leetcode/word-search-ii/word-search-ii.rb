0ec3b4243a8be946960e846855563fc29a6df2bc
Eliah Kagan <degeneracypressure@gmail.com>
2021-10-09 14:07:40 -0400

    found and fixed bug
    The bug was due to parent = child only running on one branch of the
    if in build_trie.
    
    This commit contains instrumentation (debug printing) used to find
    the bug. I plan to use it to do some other checks, then remove it.

--- leetcode/word-search-ii/word-search-ii.rb
+++ leetcode/word-search-ii/word-search-ii.rb
@@ -6,7 +6,13 @@
 # @param {String[]} words
 # @return {String[]}
 def find_words(board, words) # Note: Temporarily mutates board.
-  root = build_trie(p possible_words(board, words))
+  possible = possible_words(board, words)
+  puts "#{possible.size} possible word(s)."
+  root = build_trie(possible_words(board, words))
+  #p root.values.map { |node| node.key?(:population) }
+  pp root
+  #pp root
+  #puts "Trie population of #{root.each_value.sum { |node| node[:population] || 0 }}."
   matches = []
   height, width = dimensions(board)
 
@@ -23,6 +29,8 @@ def find_words(board, words) # Note: Temporarily mutates board.
       count += 1
 
       if (child[:population] -= 1).zero?
+        #puts "Found last word, deleting '#{ch}' node. Parent was:"
+        #puts parent
         parent.delete(ch)
         return count
       end
@@ -76,8 +84,9 @@ def build_trie(words)
       else
         child = {population: 1}
         parent[ch] = child
-        parent = child
       end
+
+      parent = child
     end
 
     parent[:word] = word
