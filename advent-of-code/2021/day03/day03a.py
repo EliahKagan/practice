09d42a296990fe8ef60a30a26ae3fa25cd27aba8
Eliah Kagan <degeneracypressure@gmail.com>
2021-12-09 22:45:19 -0500

    Advent of Code, day 3, part A

---
+++ advent-of-code/2021/day03/day03a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Advent of Code, day 3, part A"""
+
+import fileinput
+
+
+def majority(col):
+    """Gets the majority bit of a column."""
+    double_pop = sum(col) * 2
+    if double_pop == len(col):
+        raise ValueError('equally many ones and zeros')
+    return int(double_pop > len(col))
+
+
+def binary(bits):
+    """Converts a sequence of ones and zeros to a binary number."""
+    return int(''.join(map(str, bits)), base=2)
+
+
+def run():
+    """Solves the problem as described in input on stdin or a file."""
+    rows = (map(int, line.strip()) for line in fileinput.input())
+    cols = list(zip(*rows))
+    gamma = [majority(col) for col in cols]
+    epsilon = [int(not bit) for bit in gamma]
+    print(binary(gamma) * binary(epsilon))
+
+
+if __name__ == '__main__':
+    run()
