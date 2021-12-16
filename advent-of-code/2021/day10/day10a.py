154ddadada219fb3edabe0784a52eb9fac6b9749
Eliah Kagan <degeneracypressure@gmail.com>
2021-12-16 15:22:11 -0500

    Advent of Code 2021, day 10, part A

---
+++ advent-of-code/2021/day10/day10a.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""Advent of Code 2021, day 10, part A"""
+
+import fileinput
+
+from bidict import bidict
+from typeguard import typechecked
+
+
+BRACKETS = bidict({'(': ')', '[': ']', '{': '}', '<': '>'})
+
+WEIGHTS = {None: 0, ')': 3, ']': 57, '}': 1197, '>': 25137}
+
+
+@typechecked
+def find_unmatched_closer(text: str) -> str | None:
+    """
+    Finds the first unmatched closing punctuator in a string of punctuation.
+
+    Returns the character if one was found, or None if there were no
+    mismatches. Raises ValueError if any character appears in the text other
+    than known opening/closing characters.
+    """
+    # pylint: disable=unsupported-membership-test
+    # pylint: disable=unsubscriptable-object
+
+    stack = []
+
+    for ch in text:  # pylint: disable=invalid-name
+        if ch in BRACKETS:
+            stack.append(ch)
+        elif ch not in BRACKETS.inverse:
+            raise ValueError(f'unknown punctuator {ch!r}')
+        elif stack and stack[-1] == BRACKETS.inverse[ch]:
+            del stack[-1]
+        else:
+            return ch
+
+    return None
+
+
+@typechecked
+def run() -> None:
+    """Reads the input and outputs the total syntax-error score."""
+    lines = map(str.strip, fileinput.input())
+    print(sum(WEIGHTS[find_unmatched_closer(line)] for line in lines))
+
+
+if __name__ == '__main__':
+    run()
