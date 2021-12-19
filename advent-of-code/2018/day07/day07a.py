4bad9a705ac913af6935f2a6a156a5e3a0fffe84
Eliah Kagan <degeneracypressure@gmail.com>
2021-12-19 11:42:30 -0500

    Advent of Code 2018, day 7, part A

---
+++ advent-of-code/2018/day07/day07a.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+"""
+Advent of Code 2018, day 7, part A
+
+Lexicographically minimal topological sort, storing roots (as they are
+discovered) in a binary minheap.
+"""
+
+from __future__ import annotations
+
+import fileinput
+import heapq
+import re
+from typing import Any, Iterable, Mapping, NamedTuple, TypeVar
+
+from typeguard import typechecked
+
+
+_T = TypeVar('_T')
+
+
+@typechecked
+def _get_zeros(mapping: Mapping[_T, Any]) -> list[_T]:
+    """Returns the keys that are mapped to falsy values in the mapping."""
+    return [key for key, value in mapping.items() if not value]
+
+
+@typechecked
+class Edge(NamedTuple):
+    """A directed edge whose vertices are strings."""
+
+    src: str
+    """The source (tail) vertex."""
+
+    dest: str
+    """The destination (head) vertex."""
+
+
+@typechecked
+class Graph:
+    """An unweighted directed graph, representing dependencies."""
+
+    __slots__ = ('_adj', '_indegrees')
+
+    _adj: dict[str, list[str]]
+    _indegrees: dict[str, int]
+
+    @classmethod
+    def from_edges(cls, edges: Iterable[Edge]) -> Graph:
+        """Creates a directed graph from an iterable of edges."""
+        graph = cls()
+        for src, dest in edges:
+            graph.add_edge(src, dest)
+        return graph
+
+    def __init__(self):
+        """Creates an initially empty directed graph."""
+        self._adj = {}
+        self._indegrees = {}
+
+    @property
+    def order(self) -> int:
+        """The number of vertices in the graph."""
+        return len(self._adj)
+
+    def add_vertex(self, vertex: str) -> None:
+        """Adds a vertex to the graph, if it is not already present."""
+        if vertex not in self._adj:
+            self._adj[vertex] = []
+            self._indegrees[vertex] = 0
+
+    def add_edge(self, src: str, dest: str) -> None:
+        """
+        Adds a directed edge to the graph.
+
+        If one or both vertices are not in the graph, they are added too.
+        If this edge already exists in the graph, a parallel edge is added.
+        """
+        self.add_vertex(src)
+        self.add_vertex(dest)
+        self._adj[src].append(dest)
+        self._indegrees[dest] += 1
+
+    def toposort(self) -> list[str]:
+        """
+        Emits a lexicographically minimal topological sort.
+
+        Raises ValueError if the graph is cyclic.
+        """
+        indegrees = self._indegrees.copy()
+        roots = _get_zeros(indegrees)
+        heapq.heapify(roots)
+        out = []
+
+        while roots:
+            src = heapq.heappop(roots)
+            out.append(src)
+            for dest in self._adj[src]:
+                indegrees[dest] -= 1
+                if indegrees[dest] == 0:
+                    heapq.heappush(roots, dest)
+
+        if len(out) != self.order:
+            raise ValueError('cyclic graph has no topological ordering')
+
+        return out
+
+
+_STEP_PARSER = re.compile(
+    r'^Step (\w+) must be finished before step (\w+) can begin\.$')
+
+
+@typechecked
+def read_edges() -> Iterable[Edge]:
+    """Reads steps from stdin or a file as directed edges."""
+    raw_lines: Iterable[str] = fileinput.input()
+
+    for line in map(str.strip, raw_lines):
+        if not line:
+            continue
+
+        match = _STEP_PARSER.fullmatch(line)
+        if match is None:
+            raise ValueError(f'malformed line: {line}')
+
+        yield Edge._make(match.groups())
+
+
+@typechecked
+def run() -> None:
+    """Reads steps from stdin or a file. Outputs the appropriate ordering."""
+    sequence = Graph.from_edges(read_edges()).toposort()
+
+    if any(len(vertex) != 1 for vertex in sequence):
+        print(sequence)
+    else:
+        print(''.join(sequence))
+
+
+if __name__ == '__main__':
+    run()
