37afd2da6de65a35de5525aaaaa47d44e8cde8ba
Eliah Kagan <degeneracypressure@gmail.com>
2022-06-25 22:35:25 -0400

    Kruskal (MST): Really Special Subtree, in Java

---
+++ hackerrank/algorithms/graph-theory/kruskalmstrsub/kruskalmstrsub.java
@@ -0,0 +1,108 @@
+// Kruskal (MST): Really Special Subtree - Kruskal's algorithm
+// https://www.hackerrank.com/challenges/kruskalmstrsub
+
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/** Disjoint-set union data structure over a range of nonnegative integers. */
+final class DisjointSets {
+    /**
+     * Creates a disjoint-set union with singletons 0 to elementCount - 1.
+     * @param elementCount  The number of makeset operations to perform.
+     */
+    DisjointSets(int elementCount) {
+        _parents = IntStream.range(0, elementCount).toArray();
+        _ranks = new int[elementCount];
+    }
+
+    /**
+     * Joins two elements' sets, if they are not already the same.
+     * @param elem1  The first element whose set participates in the union.
+     * @param elem2  The second element whose set participates in the union.
+     * @return true if the sets were separate, false otherwise
+     */
+    boolean union(int elem1, int elem2) {
+        // Find the ancestors and stop if they are already the same.
+        elem1 = findSet(elem1);
+        elem2 = findSet(elem2);
+        if (elem1 == elem2) return false;
+
+        // Unite by rank.
+        if (_ranks[elem1] < _ranks[elem2]) {
+            _parents[elem1] = elem2;
+        } else {
+            if (_ranks[elem1] == _ranks[elem2]) ++_ranks[elem1];
+            _parents[elem2] = elem1;
+        }
+
+        return true;
+    }
+
+    private int findSet(int elem) {
+        var leader = elem;
+
+        // Find the ancestor.
+        while (leader != _parents[leader]) leader = _parents[leader];
+
+        // Compress the path.
+        while (elem != leader) {
+            var parent = _parents[elem];
+            _parents[elem] = leader;
+            elem = parent;
+        }
+
+        return leader;
+    }
+
+    private final int[] _parents;
+
+    private final int[] _ranks;
+}
+
+/** A weighted edge in an undirected graph. */
+final class Edge {
+    /** Creates an edge with endpoints u and v and weight w. */
+    Edge(int u, int v, int weight) {
+        this.u = u;
+        this.v = v;
+        this.weight = weight;
+    }
+
+    /** A vertex, the first endpoint that was specified. */
+    final int u;
+
+    /** A vertex, the second endpoint that was specified. */
+    final int v;
+
+    /** The weight on this edge. */
+    final int weight;
+}
+
+final class Solution {
+    public static void main(String[] args) {
+        try (var scanner = new Scanner(System.in)) {
+            var vertexCount = scanner.nextInt();
+            var edgeCount = scanner.nextInt();
+            var sets = new DisjointSets(vertexCount + 1);
+
+            var totalWeight = Stream.generate(() -> readEdge(scanner))
+                .limit(edgeCount)
+                .sorted(Comparator.comparingInt(edge -> edge.weight))
+                .sequential()
+                .filter(edge -> sets.union(edge.u, edge.v))
+                .mapToInt(edge -> edge.weight)
+                .sum();
+
+            System.out.println(totalWeight);
+        }
+    }
+
+    private static Edge readEdge(Scanner scanner) {
+        var u = scanner.nextInt();
+        var v = scanner.nextInt();
+        var weight = scanner.nextInt();
+        return new Edge(u, v, weight);
+    }
+}
