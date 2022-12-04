336c811cdb1d73e0f5bb5c351667309927fe6425
Eliah Kagan <degeneracypressure@gmail.com>
2022-12-04 16:14:51 -0500

    AoC 2022 day 4 part B

---
+++ advent-of-code/2022/day04/day04b.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Advent of Code 2022, day 4, part B
+
+$VERBOSE = true
+
+def parse_range(hyphenated)
+  first, last = hyphenated.split('-').map(&:to_i)
+  first..last
+end
+
+def parse_line(line)
+  line.split(',').map { |side| parse_range(side) }
+end
+
+def disjoint?(range1, range2)
+  range1.last < range2.first || range2.last < range1.first
+end
+
+def run
+  count = ARGF.each_line
+              .map(&:strip)
+              .reject(&:empty?)
+              .map { |line| parse_line(line) }
+              .count { |ranges| !disjoint?(*ranges) }
+
+  puts count
+end
+
+run if $PROGRAM_NAME == __FILE__
