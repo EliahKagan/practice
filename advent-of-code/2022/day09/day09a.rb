9f0eb10d9349b338540e7097702a8244f246e472
Eliah Kagan <degeneracypressure@gmail.com>
2022-12-09 13:31:31 -0500

    Start on AoC 2022 day 9

---
+++ advent-of-code/2022/day09/day09a.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Advent of Code, day 9, part A
+
+$VERBOSE = true
+
+require 'set'
+
+DIRECTIONS = {
+  'L' => [0, -1].freeze,
+  'R' => [0, +1].freeze,
+  'U' => [-1, 0].freeze,
+  'D' => [+1, 0].freeze
+}.freeze
+
+def run
+  tail_history = [[0, 0].freeze].to_set
+  head_row = head_col = tail_row = tail_col = 0
+
+  ARGF.map(&:strip).reject(&:empty?).map(&:split).each do |direction, count|
+    head_row_delta, head_col_delta = DIRECTIONS.fetch(direction)
+
+    count.to_i.times do
+      head_row += head_row_delta
+      head_col += head_col_delta
+      row_sep = head_row - tail_row
+      col_sep = head_col - tail_col
+      raise "Bug: excessive row separation of #{row_sep} " if row_sep.abs > 2
+      raise "Bug: excessive column separation of #{col_sep}" if col_sep.abs > 2
+
+      # FIXME: This performs too few diagonal moves.
+      tail_row += row_sep / row_sep.abs if row_sep.abs > 1
+      tail_col += col_sep / col_sep.abs if col_sep.abs > 1
+      tail_history.add([tail_row, tail_col].freeze)
+    end
+  end
+
+  puts tail_history.size
+end
+
+run if $PROGRAM_NAME == __FILE__
