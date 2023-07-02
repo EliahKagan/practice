6aa6c6bfdf3674a4b21449c63a6be3b250a97e89
Eliah Kagan <degeneracypressure@gmail.com>
2023-07-02 18:45:34 -0400

    Sliding Window Maximum, faster algorithm
    Also passes.

---
+++ leetcode/sliding-window-maximum/sliding-window-maximum-fast.cpp
@@ -0,0 +1,40 @@
+// LeetCode #239: Sliding Window Maximum
+// https://leetcode.com/problems/sliding-window-maximum/
+// Discarding smaller elements that precede larger ones.
+
+class Solution {
+public:
+    [[nodiscard]] static vector<int>
+    maxSlidingWindow(const vector<int>& nums, int k) noexcept;
+};
+
+namespace {
+    using It = vector<int>::const_iterator;
+}
+
+vector<int>
+Solution::maxSlidingWindow(const vector<int>& nums, const int k) noexcept
+{
+    auto window = deque<It>{};
+
+    const auto push = [&window](const It pos) noexcept {
+        while (!empty(window) && *window.back() < *pos) window.pop_back();
+        window.push_back(pos);
+    };
+
+    auto left = cbegin(nums);
+    auto right = left;
+    for (const auto stop = left + k; right != stop; ++right) push(right);
+
+    auto maxima = vector<int>{};
+    maxima.reserve(size(nums) - k + 1);
+    maxima.push_back(*window.front());
+
+    for (const auto stop = cend(nums); right != stop; ++left, ++right) {
+        if (window.front() == left) window.pop_front();
+        push(right);
+        maxima.push_back(*window.front());
+    }
+
+    return maxima;
+}
